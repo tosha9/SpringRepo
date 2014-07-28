d27c27a499f4facb0fd54bd84b039b393be5e3bb
Tosha Shah <tosha9@gmail.com>
2014-07-28 15:45:50 -0500

    Singleton Bean
    Only one instance of bean is created.

--- tutorial/src/main/java/tutorial/App.java
+++ tutorial/src/main/java/tutorial/App.java
@@ -11,14 +11,22 @@ public class App
        
         ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
         
-        Person person = (Person)context.getBean("person");
-        Address address = (Address)context.getBean("address");
+        System.out.println("Singleton bean");
         
-        person.speak();
+        Person person1 = (Person)context.getBean("person");
+        Person person2 = (Person)context.getBean("person");
         
-        System.out.println(person);
-        System.out.println(address);
+        person1.setTaxId(777);
         
+        System.out.println("person1 = "+person1);
+        System.out.println("person2 = "+person2);
+        
+        if(person1 == person2)
+        {
+            System.out.println("Address of the person bean for both instances is same");
+        }
+        
+        System.out.println("Conclusion: No matter how may times we call person Spring will rteturn same bean");
         ((FileSystemXmlApplicationContext)context).close();
         
     }
