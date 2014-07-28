1075334666803a1b3ec9aa87b3ef560e2e4b5292
Tosha Shah <tosha9@gmail.com>
2014-07-28 14:34:34 -0500

    SpringDemo
    udemy

---
+++ tutorial/src/main/java/tutorial/Person.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class Person
+{
+    public Person()
+    {
+       System.out.println("I am a constructor");
+    }
+    
+    public void speak ()
+    {
+        System.out.println("I am a person");
+    }
+}
