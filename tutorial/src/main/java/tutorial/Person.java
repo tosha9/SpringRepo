98bdd9e459aa1adf95f578cd3a1745792c58d367
Tosha Shah <tosha9@gmail.com>
2014-07-28 15:22:07 -0500

    Setting Bean Property

--- tutorial/src/main/java/tutorial/Person.java
+++ tutorial/src/main/java/tutorial/Person.java
@@ -5,6 +5,8 @@ public class Person
     private int id;
     private String name;
 
+    public int taxId;
+    
     public Person(int id, String name)
     {
         this.id = id;
@@ -44,8 +46,14 @@ public class Person
     @Override
     public String toString ()
     {
-        return "Person [id=" + id + ", name=" + name + "]";
+        return "Person [id=" + id + ", name=" + name + " taxId = "+taxId+"]";
     }
+
+    public void setTaxId (int taxId)
+    {
+        this.taxId = taxId;
+    }
+    
     
     
 }
